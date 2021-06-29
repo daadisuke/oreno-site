933bf1e57da71a1dc64214aa93f64b6682ba841e
伊藤　大輔 <itou_d@elf.coara.or.jp>
2021-06-29 12:37:14 +0900

    全体的に調整

--- js/viewport.js
+++ js/viewport.js
@@ -1,5 +1,5 @@
 if (navigator.platform == "iPad" || (navigator.platform == "MacIntel" && navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1 && navigator.standalone !== undefined) || (navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') == -1)) {
-    document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=1360">');
+    document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=1500">');
 } else {
     document.head.insertAdjacentHTML('beforeend', '<meta name="viewport" content="width=device-width,initial-scale=1">');
 }
