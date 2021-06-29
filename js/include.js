a78f02f3856f416300a2b30028f3b63bc3b72eb6
伊藤　大輔 <itou_d@elf.coara.or.jp>
2021-06-29 14:53:38 +0900

    ヘッダーフッターインクルード箇所修正

--- js/include.js
+++ js/include.js
@@ -1,3 +1,25 @@
-// $.ajaxSetup({cache:false});
-$("header").load("../_include/header.html");
-$("footer").load("../_include/footer.html");
+function header(rootDir){
+    $.ajax({
+        url: rootDir + "_include/header.html", // ディレクトリー変更
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); 
+            document.write(html);
+        }
+    });
+}
+
+function footer(rootDir){
+    $.ajax({
+        url: rootDir + "_include/footer.html", // ディレクトリー変更
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); 
+            document.write(html);
+        }
+    });
+}
