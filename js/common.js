062dadea5453839ea6d9a3f0481f0bcfe3d1363f
伊藤　大輔 <itou_d@elf.coara.or.jp>
2021-07-12 11:59:58 +0900

    追従ヘッダー、アンカーリンク位置調整ver作成

--- js/common.js
+++ js/common.js
@@ -43,15 +43,64 @@ $(function ($) {
 
 $(window).on('load resize', function () {
 
-    if ($(window).width() >= 768) { 
-        $(".wrap-sp-navi").removeAttr("style");
+    if ($(window).width() > 768) { 
+        $("#navi").removeAttr("style");
     } else if ($(window).width() < 768) { 
     } 
     
 });
 
-// // // メニュー用js // // //
+// ヘッダー追従
+$(function($) {
+    
+    $(window).scroll(function () {
+      if($(window).scrollTop() > 200) {
+        $("#header").addClass('fixed-h');
+        if ($(window).width() > 768) { 
+            $("body").css("padding-top","171px");
+        } else if ($(window).width() <= 768) {
+            $("body").css("padding-top","57px");
+        } 
+      } else {
+        $("#header").removeClass('fixed-h');
+		$("body").css("padding-top","0px");
+      }
+    });
 
+});
+
+// アンカーリンク位置調整
+$('#contents a[href*="#"],#navi a[href*="#"]').click(function () {
+    const i = $(this).attr('href').indexOf('#');
+    if (i !== -1) {
+        const id = $(this)
+        .attr('href')
+        .substring(i)
+        .replace('.', '')
+        .replace('/', '');
+        if (id) {
+        const headerHight = $(window).width() <= 768 ? 57 : 130;
+        const position = $(id).offset().top - headerHight;
+        $('html, body').animate({ scrollTop: position }, 500, 'swing');
+
+        if ($('.btn-menu a').hasClass('open')) {
+            $('.btn-menu a').click();
+        }
+        }
+    }
+});
+  
+window.addEventListener('load', setPosition);
+
+function setPosition() {
+    if (location.hash.indexOf('#') !== -1) {
+        const headerHight = $(window).width() <= 768 ? 57 : 130;
+        const position = $(location.hash).offset().top - headerHight;
+        $('html, body').animate({ scrollTop: position }, 500, 'swing');
+    }
+}
+
+// // // メニュー用js // // //
 
 // よくある質問
 $(function() {
@@ -76,14 +125,6 @@ $(window).on('load', function () {
 	}
 	
 });
-
-// $(window).on('load', function () {
-
-//     if($(window).innerWidth() > 768) {
-//         $(".list-year").removeAttr("style");
-//     }
-	
-// });
 
 //page-top
 $(function() {
